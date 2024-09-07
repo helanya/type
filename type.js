7994ff57fceacac3fd35dd23cabca1e971e5b6f4
Lan <helan0106@163.com>
2024-09-07 21:15:51 +0800

    feat: add cursor

--- type.js
+++ type.js
@@ -1,38 +1,85 @@
-(function(root){
-  class CType {
-    constructor(selector, options = {}) {
-      this.element = document.querySelector(selector);
-      this.text = options.text || '';
-      this.speed = options.speed || 100;
-      this.loop = options.loop || false;
-      this.index = 0;
-      this.isTyping = false;
+(function (root) {
+  function insertCss(cssContent) {
+    const style = document.createElement('style')
+    style.type = 'text/css';
+
+    if (style.styleSheet) {
+      style.styleSheet.cssText = cssContent;
+    } else {
+      style.appendChild(document.createTextNode(cssContent));
     }
 
+    document.body.appendChild(style);
+  }
 
-  
+  class CType {
+    constructor(selector, options = {}) {
+      this.element = document.querySelector(selector)
+      this.text = options.text || ''
+      this.speed = options.speed || 100
+      this.isTyping = false;
+      this._init()
+    }
+
+    _init() {
+      // 插入样式
+      this._insertStyle()
+      // 插入光标
+      this._insertCursor()
+    }
+
+    // 插入光标
+    _insertCursor() {
+      const cursorSpan = document.createElement('span')
+      cursorSpan.textContent = '|'
+      cursorSpan.classList.add('ctype-cursor')
+      this.element.appendChild(cursorSpan)
+    }
+
+    // 设置定位
+    _insertStyle() {
+      this.element.style.position = 'relative'
+      insertCss(`
+        @keyframes blink {
+          0% {
+            opacity: 1;
+          } 
+          50% {
+            opacity: 0;
+          } 
+          100% {
+            opacity: 
+            1;
+          }
+        } 
+
+        .ctype-cursor {
+          -webkit-animation: blink 1s infinite; 
+          animation: blink 1s infinite; 
+          position: absolute; 
+          right: -5px;
+        }
+        `
+      )
+    }
+
     go() {
-      if (this.isTyping) return this;
-      this.isTyping = true;
+      if (this.isTyping) return this
+      this.isTyping = true
+
+      let index = 0
 
       const type = () => {
-        if (this.index < this.text.length) {
-          this.element.textContent += this.text.charAt(this.index);
-          this.index++;
-          setTimeout(type, this.speed);
-        } else if (this.loop) {
-          setTimeout(() => {
-            this.element.textContent = '';
-            this.index = 0;
-            type();
-          }, this.speed);
+        if (index < this.text.length) {
+          const textNode = document.createTextNode(this.text[index])
+          this.element.appendChild(textNode)
+          index++
+          setTimeout(type, this.speed)
         }
       }
-  
-      this.element.textContent = '';
-      type();
-  
-      return this;
+      type()
+
+      return this
     }
   }
 
