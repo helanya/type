8ab31bbe6c8f4af75722d746314afd3bf633314b
Lan <helan0106@163.com>
2024-09-07 11:47:55 +0800

    feat: 打印效果

---
+++ type.js
@@ -0,0 +1,40 @@
+(function(root){
+  class Typewriter {
+    constructor(selector, options = {}) {
+      this.element = document.querySelector(selector);
+      this.text = options.text || '';
+      this.speed = options.speed || 100;
+      this.loop = options.loop || false;
+      this.index = 0;
+      this.isTyping = false;
+    }
+
+
+  
+    go() {
+      if (this.isTyping) return this;
+      this.isTyping = true;
+
+      const type = () => {
+        if (this.index < this.text.length) {
+          this.element.textContent += this.text.charAt(this.index);
+          this.index++;
+          setTimeout(type, this.speed);
+        } else if (this.loop) {
+          setTimeout(() => {
+            this.element.textContent = '';
+            this.index = 0;
+            type();
+          }, this.speed);
+        }
+      }
+  
+      this.element.textContent = '';
+      type();
+  
+      return this;
+    }
+  }
+
+  root.Typewriter = Typewriter
+})(window)
